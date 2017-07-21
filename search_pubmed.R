6ad641aaa2cb40bbac041292be6dea426ed4985f
Pat Schloss <pschloss@umich.edu>
2017-07-21 08:19:46 -0400

    Allow for stop/restart

--- search_pubmed.R
+++ search_pubmed.R
@@ -37,33 +37,57 @@ flatten_list_items <- function(x){
 	c(pmid, year, journal)
 }
 
-terms <- c("yeast*",  "fung*",  "viral*",  "virus*",  "phage*",  "bacteriophage*",  "archaea*",
- 					"bacteri*",  "bacteria*",  "bacterio*",  "bacterior*",  "bacterios*",  "bacteriot*",
-					"bacteriov*",  "bacteriu*",  "microbi*",  "microbe*",  "microorganism*",  "pathogen*",
-					"protist*")
+search <- function(){
+	terms <- c("yeast*",  "fung*",  "viral*",  "virus*",  "phage*",  "bacteriophage*",  "archaea*",
+	 					"bacteri*",  "bacteria*",  "bacterio*",  "bacterior*",  "bacterios*",  "bacteriot*",
+						"bacteriov*",  "bacteriu*",  "microbi*",  "microbe*",  "microorganism*",  "pathogen*",
+						"protist*")
 
-search_string <- paste(paste0(terms, "[All Fields]"), collapse= " OR ")
+	search_string <- paste(paste0(terms, "[All Fields]"), collapse= " OR ")
+	r_search <- entrez_search(db="pubmed", term=search_string, use_history=TRUE)
 
-r_search <- entrez_search(db="pubmed", term=search_string, use_history=TRUE)
-
-#> r_search$count
-#[1] 3488812
-
-composite <- NULL
-chunk_size <- 1000
-
-for(chunk_start in seq(1, r_search$count, chunk_size)){
-	print(chunk_start)
-	chunk_xml <- entrez_fetch(db="pubmed", web_history=r_search$web_history, rettype='xml',
-														retmax=chunk_size, retstart=chunk_start)
-	chunk_list <- xmlToList(chunk_xml)
-	chunk_data <- lapply(chunk_list, flatten_list_items)
-	composite <- rbind(composite, do.call(rbind, chunk_data))
+	webenv <- r_search$web_history$WebEnv
+	querykey <- r_search$web_history$QueryKey
+	hits <- r_search$count
+	write(c(webenv, querykey, hits), "search_history.txt")
 }
 
-colnames(composite) <- c("pmid", "year", "journal")
-rownames(composite) <- NULL
-composite_journal_articles <- composite[!is.na(composite[,"pmid"]),]
+retrieve_records <- function(){
+	if(file.exists("temp_pmid_year_journal.tsv")){
+		composite <- read.table(file="temp_pmid_year_journal.tsv", sep='\t')
+		query_start <- nrow(composite)+1
+	} else {
+		composite <- NULL
+		query_start <- 1
+		search()
+	}
+	cached_history <- scan(file="search_history.txt", what=character(), quiet=T)
+	total_records <- as.numeric(cached_history[3])
+	search_history <- list()
+	search_history$web_history$WebEnv <- cached_history[1]
+	search_history$web_history$QueryKey <- cached_history[2]
 
-write.table(file="pmid_year_journal.tsv", x=composite_journal_articles, quote=F, row.names=F,
-						sep='\t')
+	chunk_size <- 100
+	total_records <- 1000
+
+	for(chunk_start in seq(query_start, total_records, chunk_size)){
+		print(chunk_start)
+		chunk_xml <- entrez_fetch(db="pubmed", web_history=search_history$web_history, rettype='xml',
+															retmax=chunk_size, retstart=chunk_start)
+		chunk_list <- xmlToList(chunk_xml)
+		chunk_data <- lapply(chunk_list, flatten_list_items)
+		composite <- rbind(composite, do.call(rbind, chunk_data))
+
+		write.table(file="temp_pmid_year_journal.tsv", x=composite, quote=F, row.names=F, col.names=F,
+								sep='\t')
+	}
+}
+
+clean_up <- function(){
+	final_data <- read.table(file="temp_pmid_year_journal.tsv", header=F, sep='\t')
+	final_data <- final_data[!is.na(final_data[,1]),]
+	colnames(final_data) <- c("pmid", "year", "journal")
+
+	write.table(file="pmid_year_journal.tsv", final_data, row.names=F, quote=F, sep='\t')
+	unlink("temp_pmid_year_journal.tsv")
+}
