567dba91cbcdb02d8780a5034f91c4ec8a8a6941
Pat Schloss <pschloss@umich.edu>
2017-07-21 12:56:58 -0400

    Use entrez summary approach and bigger chunks

--- search_pubmed.R
+++ search_pubmed.R
@@ -25,16 +25,15 @@ library("rentrez")
 library(XML)
 
 flatten_list_items <- function(x){
-	year <- NA
-	journal <- NA
-	pmid <- NA
+	year <- x$PubDate
+	journal <- x$FullJournalName
+	doi <- x$ELocationID
 
-	if(names(x)[1] == "MedlineCitation"){
-		year <- x$PubmedData$History$PubMedPubDate$Year
-		journal <- x$MedlineCitation$Article$Journal$Title
-		pmid <- x$MedlineCitation$PMID$text
-	}
-	c(pmid, year, journal)
+	if(is.null(doi)){ doi <- "NA" }
+	if(is.null(year)){ year <- "NA" }
+	if(is.null(journal)){ journal <- "NA" }
+
+	c(doi, year, journal)
 }
 
 search <- function(){
@@ -63,31 +62,38 @@ retrieve_records <- function(){
 	}
 	cached_history <- scan(file="search_history.txt", what=character(), quiet=T)
 	total_records <- as.numeric(cached_history[3])
-	search_history <- list()
-	search_history$web_history$WebEnv <- cached_history[1]
-	search_history$web_history$QueryKey <- cached_history[2]
+	web_history <- list()
+	web_history$WebEnv <- cached_history[1]
+	web_history$QueryKey <- cached_history[2]
 
-	chunk_size <- 100
-	total_records <- 1000
+	chunk_size <- 10000
+	# total_records <- 1000
 
 	for(chunk_start in seq(query_start, total_records, chunk_size)){
 		print(chunk_start)
-		chunk_xml <- entrez_fetch(db="pubmed", web_history=search_history$web_history, rettype='xml',
-															retmax=chunk_size, retstart=chunk_start)
-		chunk_list <- xmlToList(chunk_xml)
-		chunk_data <- lapply(chunk_list, flatten_list_items)
-		composite <- rbind(composite, do.call(rbind, chunk_data))
+		chunk_summary <- entrez_summary(db="pubmed", web_history=web_history, retmax=chunk_size,
+				retstart=chunk_start, retmode="xml")
 
-		write.table(file="temp_pmid_year_journal.tsv", x=composite, quote=F, row.names=F, col.names=F,
-								sep='\t')
+		chunk_list <- extract_from_esummary(chunk_summary, c("PubDate", "FullJournalName",
+				"ELocationID"), simplify=FALSE)
+
+		chunk_df <- data.frame(t(sapply(chunk_list, flatten_list_items)))
+
+		composite <- rbind(composite, chunk_df)
+		write.table(file="temp_pmid_doi_year_journal.tsv", x=composite, quote=F, row.names=T,
+				col.names=F, sep='\t')
 	}
 }
 
 clean_up <- function(){
-	final_data <- read.table(file="temp_pmid_year_journal.tsv", header=F, sep='\t')
+	final_data <- read.table(file="temp_pmid_doi_year_journal.tsv", header=F, sep='\t')
 	final_data <- final_data[!is.na(final_data[,1]),]
-	colnames(final_data) <- c("pmid", "year", "journal")
+	colnames(final_data) <- c("pmid", "doi", "year", "journal")
 
-	write.table(file="pmid_year_journal.tsv", final_data, row.names=F, quote=F, sep='\t')
+	final_data$year <- gsub(".*(\\d{4}).*", "\\1", final_data$year)
+	final_data$doi <- gsub(".*doi: (\\S*).*", "\\1", final_data$doi)
+
+	write.table(file="pmid_doi_year_journal.tsv", final_data, row.names=F, quote=F, sep='\t')
 	unlink("temp_pmid_year_journal.tsv")
+	unlink("search_history.txt")
 }
