79726e3fa90ca5d4270229cf30154745da4a7651
Pat Schloss <pschloss@umich.edu>
2017-08-08 09:09:27 -0400

    Adjust delay

--- search_pubmed.R
+++ search_pubmed.R
@@ -60,7 +60,7 @@ retrieve_record_chunk <- function(chunk_start, chunk_size=10000){
 
 	load("r_search.rdata")
 
-	Sys.sleep(runif(1) * 50) # slow down searches so they don't clobber server
+	Sys.sleep(runif(1) * 5) # slow down searches so they don't clobber server
 
 	chunk_summary <- rentrez::entrez_summary(db="pubmed", web_history=r_search$web_history,
 																	retmax=chunk_size, retstart=chunk_start, retmode="xml")
